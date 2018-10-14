7db645b9e6dec6adcadd5107648a35b2e43453e8
Krasimir Stoyanov <krasimir641@gmail.com>
2018-10-14 22:57:25 +0200

    Added rotation of wisp mechanic and refactored parts of the existing code

--- ProjectWisp/Assets/Scripts/MoveController.cs
+++ ProjectWisp/Assets/Scripts/MoveController.cs
@@ -2,72 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveController : MonoBehaviour {
+public class MoveController : MonoBehaviour
+{
 
-	[SerializeField]
-	[Range(0, 30)]
-	float Speed = 30.0f;
+    [SerializeField]
+    [Range(0, 30)]
+    float Speed = 30.0f;
 
-	// Animator animator;
-	SpriteRenderer spriteRenderer;
-
-
-	// Use this for initialization
-	void Start () {
-
-		//transform.position = new Vector3(0, 0, 0);
-
-		// transform.position = new Vector3(0, 0, 0);
+    // Animator animator;
+    SpriteRenderer spriteRenderer;
 
 
-		// get Access to Animator component
-		// animator = GetComponent<Animator>();
-		spriteRenderer = GetComponent<SpriteRenderer>();
+    // Use this for initialization
+    void Start()
+    {
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Vector3 currentPosition = transform.position;
-		// currentPosition.x += Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
-		// currentPosition.y += Input.GetAxis("Vertical") * Speed * Time.deltaTime;
-		// transform.position = currentPosition;
+        //transform.position = new Vector3(0, 0, 0);
 
-		// set value to characterState parameter based on input
+        // transform.position = new Vector3(0, 0, 0);
 
-		// animation
-		// if (Input.GetAxis("Horizontal") == 0){
 
-		// 	// animator.SetInteger("characterState", 0);
+        // get Access to Animator component
+        // animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
-		// 	if (Input.GetAxis("Vertical") == 0){
-		// 		animator.SetInteger("characterState", 0);
-		// 	}
-		// 	else{
-		// 		animator.SetInteger("characterState", 2);
-		// 	}
-		// }
-		// else{
-		// 	animator.SetInteger("characterState", 1);
-		// }
+    }
 
-		if (Input.GetAxis("Horizontal") > 0){
-			// spriteRenderer.flipX = true;
-			gameObject.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
-		}
-		else if(Input.GetAxis("Horizontal") < 0){
-			// spriteRenderer.flipX = false;
-			gameObject.transform.localScale = new Vector3(-1.5f, 1.5f, 1.5f);
-		}
-		
-		// if (Input.GetAxis("Vertical") > 0){
-		// 	spriteRenderer.flipY = false;
-		// }
-		// else if(Input.GetAxis("Vertical") < 0){
-		// 	spriteRenderer.flipY = true;
-		// }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentPosition = transform.position;
+        // currentPosition.x += Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        // currentPosition.y += Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+        // transform.position = currentPosition;
 
-		// movement
-		// transform.Translate(Input.GetAxis("Horizontal") * Speed * Time.deltaTime, Input.GetAxis("Vertical") * Speed * Time.deltaTime, 0);
-	}
+        // set value to characterState parameter based on input
+
+        // animation
+        // if (Input.GetAxis("Horizontal") == 0){
+
+        // 	// animator.SetInteger("characterState", 0);
+
+        // 	if (Input.GetAxis("Vertical") == 0){
+        // 		animator.SetInteger("characterState", 0);
+        // 	}
+        // 	else{
+        // 		animator.SetInteger("characterState", 2);
+        // 	}
+        // }
+        // else{
+        // 	animator.SetInteger("characterState", 1);
+        // }
+
+        //if (Input.GetAxis("Horizontal") > 0)
+        //{
+        //    // spriteRenderer.flipX = true;
+        //    gameObject.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+        //}
+        //else if (Input.GetAxis("Horizontal") < 0)
+        //{
+        //    // spriteRenderer.flipX = false;
+        //    gameObject.transform.localScale = new Vector3(-1.5f, 1.5f, 1.5f);
+        //}
+
+        // if (Input.GetAxis("Vertical") > 0){
+        // 	spriteRenderer.flipY = false;
+        // }
+        // else if(Input.GetAxis("Vertical") < 0){
+        // 	spriteRenderer.flipY = true;
+        // }
+
+        // movement
+        // transform.Translate(Input.GetAxis("Horizontal") * Speed * Time.deltaTime, Input.GetAxis("Vertical") * Speed * Time.deltaTime, 0);
+    }
 }
--- ProjectWisp/Assets/Scripts/fireballBehaviour.cs
+++ ProjectWisp/Assets/Scripts/fireballBehaviour.cs
@@ -1,23 +1,22 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
-public class fireballBehaviour : MonoBehaviour {
+public class FireballBehaviour : MonoBehaviour
+{
+    [Tooltip("The lifetime of the GameObject.")]
+    public float lifeTime = 2f;
 
-	public float lifeTime = 2f;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(DestroyFireball());
+    }
+    
+    IEnumerator DestroyFireball()
+    {
+        yield return new WaitForSeconds(lifeTime);
 
-	public Rigidbody2D rb;
-	// Use this for initialization
-	void Start () {
-		StartCoroutine(DestroyFireball());
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	IEnumerator DestroyFireball() {
-		yield return new WaitForSeconds(lifeTime);
-		Destroy(gameObject);
-	}
-} 
+        Destroy(gameObject);
+    }
+}
