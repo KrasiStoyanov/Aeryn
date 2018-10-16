a5bce86b2dd14050743ae97eb841e47cfb82fbba
MnemosyneVL <kronos2906@gmail.com>
2018-10-16 16:02:07 +0200

    claw AI ready

--- ProjectWisp/Assets/Scripts/AI/Boss/BossClawBehaviour.cs
+++ ProjectWisp/Assets/Scripts/AI/Boss/BossClawBehaviour.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class BossClawBehaviour : MonoBehaviour {
 
 	//movement
+	[SerializeField]
+	float rotationSpeed;
 
 	//attack
 	[SerializeField]
-	float timeBtwAttacks;
+	float attackCooldown;
 	[SerializeField]
-	float rotationSpeed;
+	float flyToTargetSpeed;
+	private float timeBtwAttacks = 3;
+	private bool canAttack;
+	private bool didAttack;
+	private Vector3 attackTarget;
 
 	//general
 	private Transform target;
 	private Vector3 clawInitialPosition;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
 		clawInitialPosition = transform.position;
 
@@ -25,12 +32,58 @@ public class BossClawBehaviour : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		if (timeBtwAttacks > 0)
+			{
+				Rotate();
+				timeBtwAttacks -= Time.deltaTime;
+			}
+			else 
+			{
+				Attack();
+				
+			}
 
+	}
+	//claw rotates towards character
+	void Rotate() 
+	{
 		Vector2 direction = target.position - transform.position;
 		float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg;
 		Quaternion rotation =	Quaternion.AngleAxis(angle,Vector3.forward);
 		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
-
 	}
+	//claw attacks
+	void Attack()
+	{
+		if (canAttack)
+		{
+			if (Vector2.Distance(transform.position, attackTarget) > 0 && didAttack == false)
+			{
+				transform.position = Vector2.MoveTowards(transform.position, attackTarget, flyToTargetSpeed * Time.deltaTime);
+			}
+			else 
+			{
+				didAttack = true;
+				if (Vector2.Distance(transform.position, clawInitialPosition) > 0)
+				{
+					transform.position = Vector2.MoveTowards(transform.position, clawInitialPosition, flyToTargetSpeed/2 * Time.deltaTime);
+				}
+				else
+				{
+					timeBtwAttacks = attackCooldown;
+					canAttack = false;
+					didAttack = false;
+				}
+			}
+		}
+		else
+		{
+			attackTarget = target.position;
+			canAttack = true;
+		}
+	}
+
+		
 }
