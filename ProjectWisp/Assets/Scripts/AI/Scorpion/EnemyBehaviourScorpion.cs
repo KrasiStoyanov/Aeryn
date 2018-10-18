f85fe73b0b1036c5e2115fad5753ba84edb263db
MnemosyneVL <kronos2906@gmail.com>
2018-10-19 01:09:43 +0200

    Super enemy AI

--- ProjectWisp/Assets/Scripts/AI/Scorpion/EnemyBehaviourScorpion.cs
+++ ProjectWisp/Assets/Scripts/AI/Scorpion/EnemyBehaviourScorpion.cs
@@ -13,6 +13,16 @@ public class EnemyBehaviourScorpion : MonoBehaviour {
 	[SerializeField]
 	float shootDistance;
 	[SerializeField]
+	float safeDistance;
+	private bool waitCommand;
+	public GameObject dotRight;
+	public GameObject dotLeft;
+	private Vector2 rayCasterRight;
+	private Vector2 rayCasterLeft;
+	private RaycastHit2D groundInfoLeft;
+	private RaycastHit2D groundInfoRight;
+	private RaycastHit2D wallInfoLeft;
+	private RaycastHit2D wallInfoRight;
 	
 	float retreatDistance;
 	//shooting
@@ -30,24 +40,36 @@ public class EnemyBehaviourScorpion : MonoBehaviour {
 	private Vector2 targetProjection;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		monsterPosition = GetComponent<Transform>();
 		
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		rayCasterLeft = new Vector2(dotLeft.transform.position.x, dotLeft.transform.position.y - 0.3f);
+		rayCasterRight = new Vector2(dotRight.transform.position.x, dotRight.transform.position.y - 0.3f);
+		groundInfoLeft = Physics2D.Raycast(rayCasterLeft, Vector2.down, 3f);
+		groundInfoRight = Physics2D.Raycast(rayCasterRight, Vector2.down, 3f);
+		wallInfoLeft = Physics2D.Raycast(dotLeft.transform.position, Vector2.left, 0.1f);
+		wallInfoRight = Physics2D.Raycast(dotRight.transform.position, Vector2.right, 0.1f);
 		target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
 		targetdir = target.position -transform.position;
 		targetProjection = new Vector2(target.position.x,monsterPosition.position.y);
 		
-		if (triggered == true){ 
-			if (timeBtwShots > 0){
+		if (triggered == true)
+		{ 
+			if (timeBtwShots > 0)
+			{
 				Run();
 				timeBtwShots -= Time.deltaTime;
 			}
-			else {
+			else 
+			{
+				waitCommand = false;
 				Attack();
 				
 			}
@@ -62,21 +84,36 @@ public class EnemyBehaviourScorpion : MonoBehaviour {
 	}
 
 	// Attack behaviour
-	private void Attack () {
-		if(Vector2.Distance(transform.position, targetProjection) > shootDistance){
-
-			transform.position = Vector2.MoveTowards(transform.position, targetProjection, movementSpeed * Time.deltaTime);
-		}
-		else if (Vector2.Distance(transform.position, target.position) <= shootDistance || Vector2.Distance(transform.position, targetProjection) <= shootDistance){
+	private void Attack () 
+	{
+		if (Vector2.Distance(transform.position, target.position) <= shootDistance || Vector2.Distance(transform.position, targetProjection) <= 2f || groundInfoLeft.collider == false || groundInfoRight.collider == false){
+			transform.position = Vector2.MoveTowards(transform.position, targetProjection, -movementSpeed * Time.deltaTime);
 			monsterScorpionTurret.Shoot();
 			timeBtwShots = weaponCoolDown;
 		}
-		
+		else if(Vector2.Distance(transform.position, target.position) >= shootDistance && groundInfoLeft.collider == true && groundInfoRight.collider == true){
+				transform.position = Vector2.MoveTowards(transform.position, targetProjection, movementSpeed * Time.deltaTime);
+		}
 
 	}
 	//Retreat behaviour
-	private void Run () {
-		transform.position = Vector2.MoveTowards(transform.position, targetProjection, -movementSpeed * Time.deltaTime);
+	private void Run () 
+	{
+		if (Vector2.Distance(transform.position, target.position) <= safeDistance)
+		{
+			if (wallInfoLeft.collider == false && wallInfoRight.collider == false)
+			{
+				if (groundInfoLeft.collider == true && groundInfoRight.collider == true && waitCommand == false)
+				{
+					transform.position = Vector2.MoveTowards(transform.position, targetProjection, -movementSpeed * Time.deltaTime);
+				}
+				else if (groundInfoLeft.collider == false || groundInfoRight.collider == false)
+				{
+					transform.position = Vector2.MoveTowards(transform.position, targetProjection, movementSpeed * Time.deltaTime);
+					waitCommand = true;
+				}
+			}
+		}
 	}
 	//Shooting function old
 	// private void Shoot () {
