4954b3f98908afefc3a6aa3730333f554e11b901
Krasimir Stoyanov <krasimir641@gmail.com>
2018-10-28 02:23:24 +0100

    Made the enemy shoot and added health to the wisp, need only to detect for collision with player and reduce health

--- ProjectWisp/Assets/Scripts/fireballBehaviour.cs
+++ ProjectWisp/Assets/Scripts/fireballBehaviour.cs
@@ -8,18 +8,30 @@ public class FireballBehaviour : MonoBehaviour
     [Tooltip("The lifetime of the GameObject.")]
     public float lifeTime = 2f;
 
+    [Tooltip("Reference to the main character.")]
+    public WispManager wispManager;
+
     // Use this for initialization
     void Start()
     {
+        // Get all enemies and exclude them from collision detection
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject enemy in enemies)
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), enemy.GetComponent<Collider2D>());
+        }
+
         StartCoroutine(DestroyFireball());
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag != "Player")
+        if (collision.collider.tag == "Player")
         {
-            Destroy(gameObject);
+            wispManager.ChangeHealth(-10);
         }
+
+        Destroy(gameObject);
     }
 
     IEnumerator DestroyFireball()
