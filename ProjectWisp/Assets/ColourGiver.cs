e5d0405f6ba9c4aec8c302865a8c130a49443ec8
Robin Potze <robin-potze@live.nl>
2018-10-30 00:54:44 +0100

    Added Pause Menu Elements
    Made them in 3 different scales, Still need to work on implementing everything. Also changed up the Wisps hair, although I feel like there is something more missing. Something that creates a connection between player and character. Something recognizable.

---
+++ ProjectWisp/Assets/ColourGiver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourGiver : MonoBehaviour {
+
+	public Gradient gradient;
+	// Use this for initialization
+	void Start () {
+		// this.GetComponent<SpriteRenderer>().material.SetColor("_Color", gradient);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
