dc6b6b63cebf2d1ec4d39cb2237197de9db618e7
MnemosyneVL <kronos2906@gmail.com>
2018-10-31 11:35:03 +0100

    Eyes glowing + Charge limit

--- ProjectWisp/Assets/Scripts/ChargeComponent.cs
+++ ProjectWisp/Assets/Scripts/ChargeComponent.cs
@@ -6,28 +6,36 @@ public class ChargeComponent : MonoBehaviour {
 
     public Gradient gradient;
     public float duration;
-    float t = 0f;
-	 
+    private float t = 0f;
+	private SpriteRenderer eyeColor;
+	private Color initialColor; 
+	private float percentValue;
 	// Use this for initialization
 	void Start () {
+		eyeColor = GetComponent<SpriteRenderer>();
+		initialColor = eyeColor.color;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// now if the player is holding the button
-		if(Input.GetMouseButtonDown(0)) 
+		if(Input.GetMouseButton(0)) 
 		{	
 			// // make sure her eyes glow red to indicate the charge
 			// eyeColour = Color.Lerp(Color.blue, Color.red, Time.deltaTime);
 			// //Can't have pretty eyes without any colour
 			// GameObject.Find("Eye").GetComponent<SpriteRenderer>().color = eyeColour;
-			
-			float value = Mathf.Lerp(0f, 1f, t);
-        	t += Time.deltaTime / duration;
-     		Color color = gradient.Evaluate(value);
-        	GameObject.Find("Eye").GetComponent<SpriteRenderer>().color = color;
 
+			
+			Color colorValue = Color.Lerp(gradient.Evaluate(0f), gradient.Evaluate(1f), t);
+			t += Time.deltaTime/duration;
+        	eyeColor.color = colorValue;
+
+		}
+		else if (Input.GetMouseButtonUp(0))
+		{
+			eyeColor.color = initialColor;
 		}
 	}
 }
